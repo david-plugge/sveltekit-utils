65f7148472206197ca841e8a101de78c1aaae227
David Plugge <git@davidplugge.de>
2024-03-17 17:23:41 +0100

    update

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <div class="flex gap-2">
-	{#each { length: 10 } as item, i}
+	{#each { length: 10 } as _item, i}
 		<a class="bg-neutral-50 px-4 py-2" href="/posts/{i}" use:shallow.link={i}> {i} </a>
 	{/each}
 </div>
